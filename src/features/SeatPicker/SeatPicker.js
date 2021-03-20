302de608e2248ca3f228f34727911af8300f6886
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-03-20 20:56:23 +0700

    update seat picker page layout

--- src/features/SeatPicker/SeatPicker.js
+++ src/features/SeatPicker/SeatPicker.js
@@ -41,27 +41,35 @@ const SeatPicker = () => {
     gridTemplateColumns: `repeat(${bus.col}, 50px)`,
   };
 
-  if (loading) {
-    return <div>Please Wait</div>;
-  }
-
   return (
     <div className="seatpicker">
-      <h1>Seat Picker</h1>
-      <div className="seatpicker__grid_container" style={grid}>
-        {Array.apply(null, Array(item)).map((e, i) => (
-          <SeatPickerItem
-            key={i}
-            seatId={seat[i].seat_id}
-            seatKey={i}
-            disabled={seat[i].disable === 1}
-            ticket_id={seat[i].ticket_id}
-            selected={selected.some(
-              (element) => element.seat_id === seat[i].seat_id
-            )}
-            handleClick={handleClick}
-          />
-        ))}
+      <div className="seatpicker__title_container">
+        <h1 className="seatpicker__title">Seat Picker</h1>
+      </div>
+      <div className="seatpicker__description">
+        <p>This is a seat layout sample with 5 columns and 10 rows.</p>
+        <p>Click the seat below to buy a ticket for the specific seat.</p>
+      </div>
+      <div className="seatpicker__content">
+        {loading ? (
+          <div>Please wait...</div>
+        ) : (
+          <div className="seatpicker__grid_container" style={grid}>
+            {Array.apply(null, Array(item)).map((e, i) => (
+              <SeatPickerItem
+                key={i}
+                seatId={seat[i].seat_id}
+                seatKey={i}
+                disabled={seat[i].disable === 1}
+                ticket_id={seat[i].ticket_id}
+                selected={selected.some(
+                  (element) => element.seat_id === seat[i].seat_id
+                )}
+                handleClick={handleClick}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
