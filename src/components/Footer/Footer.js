6b7b360fb3887d287f21aa233feda1ab77998329
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-03-24 08:24:48 +0700

    add footer and some layout margin adjustment

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Built with ❤️ by Raindy
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../features/Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Page = ({ children }) => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 };
